6353cbe51daf95e23717f801cb7d29843a4e30a4
Sarah Eggermont <sarah.eggermont@student.hogent.be>
2024-01-02 11:14:11 +0100

    Delete documentation from this repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,10 +52,6 @@ android {
     }
 }
 
-tasks.dokkaHtml.configure {
-    outputDirectory.set(file("../documentation/html"))
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
